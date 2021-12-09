4d902045979ad2121d5efb076a358a64303e3b3b
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-12-09 17:21:11 +0100

    FIX - Forgot to update everything to milestone 2

---
+++ Milestone_2/assets/js/app.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+
+    el: "#app",
+    data: {
+        disks: null
+    },
+    mounted() {
+        axios
+            .get("/api/disks.php")
+            .then(r => {
+                this.disks = r.data
+            }).catch(e => {
+                console.log(e);
+            })
+    }
+
+})
